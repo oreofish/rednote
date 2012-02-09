68a1da8f9df64e0b764110af78a3499d5a1977d5
Rick Xing <jianxing@redflag-linux.com>
2012-02-09 16:30:32 +0800

    INIT
    rails new rednote -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rednote::Application.config.secret_token = '5febaeb5797756891d150c118b0a997e93d2dd9c63607cb76cb91f447eddd7a8ef6ad188ce9252c20935fa3b47e5690624586b7c010c9ee2c27923aa2d483389'
