4b208d35102c7534176d3637263f49d3cb350d32
Rick Xing <jianxing@redflag-linux.com>
2012-02-10 10:17:17 +0800

    fix models of ideas and users

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :integer(4)      not null, primary key
+#  user_id    :integer(4)
+#  content    :text
+#  image      :string(255)
+#  link       :string(255)
+#  type       :integer(4)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Note < ActiveRecord::Base
+  validates :content, :presence => true
+
+  validates :user_id, :presence => true
+
+  belongs_to :user
+  attr_accessible :content, :image
+
+
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :integer(4)      not null, primary key
+#  user_id    :integer(4)
+#  content    :text
+#  image      :string(255)
+#  link       :string(255)
+#  type       :integer(4)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Note do
