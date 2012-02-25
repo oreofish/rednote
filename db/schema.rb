76c59cfb000cc9f111ef20c58380245295b70c49
shensuwen <shensuwen@redflag-linux.com>
2012-02-25 11:23:38 -0500

    fix the bug of edit_password page

--- db/schema.rb
+++ db/schema.rb
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(:version => 20120223144911) do
     t.integer  "user_id"
     t.text     "summary"
     t.integer  "kind",        :default => 0
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "description"
     t.string   "upload"
   end
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(:version => 20120223144911) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "",                      :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "",                      :null => false
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -80,10 +80,10 @@ ActiveRecord::Schema.define(:version => 20120223144911) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                                                 :null => false
-    t.datetime "updated_at",                                                                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "nickname"
-    t.string   "avatar",                                :default => "/images/icons/00.jpeg"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
