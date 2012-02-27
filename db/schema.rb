43490ec990457df05042df18c8e0f3ab439a2d2a
Sian Cao <sycao@redflag-linux.com>
2012-02-27 19:05:48 +0800

    hot fix: path error

--- db/schema.rb
+++ db/schema.rb
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(:version => 20120226112454) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",                      :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",                      :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(:version => 20120226112454) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nickname"
-    t.string   "avatar"
+    t.string   "avatar",                                :default => "/images/icons/00.jpeg"
     t.string   "preview"
   end
 
