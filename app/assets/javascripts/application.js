f331d6fff7e1e0458e3c7b974d9aff41fadd901c
zhouhuan <zhouhuan@redflag-linux.com>
2012-02-28 16:42:03 +0800

    make jcrop work well

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -119,36 +119,4 @@ $(document).ready( function() {
     scrolltop();
     commentsManager.bindHandlers();
     time.updateTime();
-
-    $('#cropbox').Jcrop({
-        onChange: update_crop,
-        onSelect: update_crop,
-        setSelect: [300, 200, 200, 300],
-        aspectRatio: 1
-    });
-
-    function update_crop(coords) {
-        var rx = 100/coords.w;
-        var ry = 100/coords.h;
-        var lw = $('#cropbox').width();
-        var lh = $('#cropbox').height();
-        var ratio = $('#cropbox').attr('date') / lw ;
-
-        $('#preview').css({
-            width: Math.round(rx * lw) + 'px',
-            height: Math.round(ry * lh) + 'px',
-            marginLeft: '-' + Math.round(rx * coords.x) + 'px',
-            marginTop: '-' + Math.round(ry * coords.y) + 'px'
-        });
-        $("#crop_x").val(Math.round(coords.x * ratio));
-        $("#crop_y").val(Math.round(coords.y * ratio));
-        $("#crop_w").val(Math.round(coords.w * ratio));
-        $("#crop_h").val(Math.round(coords.h * ratio));
-    }
-
-    var form = $('#preview_form').find('.edit_user');
-    $('#user_preview').bind('change',function() {
-        form.submit();
-    });
-
 } );
