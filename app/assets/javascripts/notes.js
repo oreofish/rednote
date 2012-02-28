c9d38422af689a82e7c7b1651d197bad4483d2c3
Sian Cao <sycao@redflag-linux.com>
2012-02-28 13:28:33 +0800

    adjust style of new_note forms
    add show handler to reset content of forms
    make controls look nicer

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,6 +1,19 @@
 $(function() {
+
     if (location.pathname === "/" || location.pathname === "/notes") {
         rednote.pager('/notes/page').start();
+
+        $('ul.nav-pills').on('show', function (e) {
+            console.log('change' + e.target.toString());
+            var nowtab = e.target // activated tab
+            var id = $(nowtab).attr('href').substr(1);
+            $('#'+id).find('#new_note')[0].reset();
+            if (id === "text") {
+                if (CKEDITOR.instances["note_code"]) {
+                    CKEDITOR.instances["note_code"].setData('');
+                }
+            }
+        });
 
     } else if (location.pathname.match(/^\/users\//)) {
         rednote.pager('/users/page').start();
