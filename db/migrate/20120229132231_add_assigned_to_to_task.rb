cb031cf6ce29454a8317213ff372de44929a29db
Rick Xing <jianxing@redflag-linux.com>
2012-03-01 13:31:07 +0800

    update tasks table

--- db/migrate/20120229132231_add_assigned_to_to_task.rb
+++
@@ -1,6 +0,0 @@
-class AddAssignedToToTask < ActiveRecord::Migration
-  def change
-    add_column :tasks, :assigned_to, :integer
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(:version => 20120229132231) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "assigned_to"
+    t.datetime "start_at"
+    t.datetime "finish_at"
   end
 
   create_table "users", :force => true do |t|
