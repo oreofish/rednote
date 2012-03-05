2c460d8b160867cca24c0006cc75c102da143fa8
zhouhuan <zhouhuan@redflag-linux.com>
2012-03-05 15:25:39 +0800

    change rednote_data

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120304080220) do
 
-  create_table "add_status_to_tasks", :force => true do |t|
-    t.integer  "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "books", :force => true do |t|
     t.string   "title"
     t.datetime "created_at",                     :null => false
@@ -114,8 +108,8 @@ ActiveRecord::Schema.define(:version => 20120304080220) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
