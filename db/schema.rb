af91376c7a61de1c77982ce477cbdda0c51ad263
Rick Xing <oreofish@gmail.com>
2012-03-21 09:59:41 +0800

    fix bootstrap bug: regenerate bootstrap:install

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320025821) do
+ActiveRecord::Schema.define(:version => 20120320033207) do
 
   create_table "answers", :force => true do |t|
     t.integer  "user_id"
-    t.integer  "question_id"
     t.integer  "score"
     t.text     "content"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "question_id"
   end
 
   create_table "attachements", :force => true do |t|
