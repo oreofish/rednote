2c1d6733d593502052063d6cb6989dfa9b0a455c
Rick Xing <oreofish@gmail.com>
2012-03-30 15:26:42 +0800

    Revert "init"
    This reverts commit b3d4d1006c5451bb6abca5c7c9c79c65a974575e.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rhnote::Application.config.secret_token = '8aae4289713ad38a6fa29432ec58a65fb3a9377f010bcd4a42f3f4dd4dacac257276096541b6d4fdf81e81a9c788c155221471a21c2572de565ce561952a3619'
